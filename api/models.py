3f4a179dbddd48189f9452e3d4c4bfe9a4f68441
arnold.chan <arnold.chan@lndata.com>
2022-09-23 13:41:26 +0700

    feat(score): :sparkles: add score validation

--- api/models.py
+++ api/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 
@@ -21,7 +22,12 @@ class Course(models.Model):
     credits = models.BigIntegerField()
 
 
+def validate_score(value):
+    if value > 100:
+        raise ValidationError("value can only below 100")
+
+
 class StudentCourseMapping(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    score = models.PositiveIntegerField()
+    score = models.PositiveIntegerField(validators=[validate_score])
