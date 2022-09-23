fff3d59cf64d5f99f53391bd48a30357786d2cf0
arnold.chan <arnold.chan@lndata.com>
2022-09-23 15:29:32 +0700

    feat(pagination): :sparkles: use PageNumberPagination

--- project/settings.py
+++ project/settings.py
@@ -130,4 +130,6 @@ REST_FRAMEWORK = {
     'DEFAULT_VERSION': 'v1.0',
     'ALLOWED_VERSIONS': ('v1.0'),
     'VERSION_PARAMS': 'version',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
